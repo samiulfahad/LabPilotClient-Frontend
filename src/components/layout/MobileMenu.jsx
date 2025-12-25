746491be8930a0f9a46a041fd5e0bb01b081d8af
Samiul Fahad <80414173+samiulfahad@users.noreply.github.com>
2025-12-26 04:25:01 +0600

    Update MobileMenu.jsx

--- src/components/layout/MobileMenu.jsx
+++ src/components/layout/MobileMenu.jsx
@@ -137,7 +137,7 @@ const MobileMenu = () => {
 
             <button
               onClick={toggleMenu}
-              className="w-9 h-9 flex items-center justify-center rounded-xl bg-gray-600 hover:bg-gray-700 transition-all duration-200 border border-gray-200 shadow-sm"
+              className="w-9 h-9 flex items-center justify-center rounded-xl bg-blue-500 hover:bg-blue-700 transition-all duration-200 border border-gray-200 shadow-sm"
             >
               <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
