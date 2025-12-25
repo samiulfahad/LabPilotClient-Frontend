fffa780e30f9cdb9795be2658c5ce8f74acd5fdc
Samiul Fahad <80414173+samiulfahad@users.noreply.github.com>
2025-12-26 03:48:21 +0600

    Update MobileMenu.jsx

--- src/components/layout/MobileMenu.jsx
+++ src/components/layout/MobileMenu.jsx
@@ -124,10 +124,10 @@ const MobileMenu = () => {
         {/* Flex container for proper scrolling */}
         <div className="flex flex-col h-full">
           {/* Header - Fixed */}
-          <div className="flex-shrink-0 flex items-center justify-between p-4 border-b border-gray-100 bg-gradient-to-r from-sky-300    to-white">
+          <div className="flex-shrink-0 flex items-center justify-between p-4 border-b border-gray-100 bg-sky-200">
             <div className="flex flex-col items-center space-x-3">
-               <div className="w-20 h-10 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center shadow-sm">
-                <span className="text-white font-bold text-sm">Lab Pilot</span>
+               <div className="w-20 h-10 bg-gradient-to-br from-gray-100 to-gray-200 rounded-2xl flex items-center justify-center shadow-sm">
+                <span className="text-blue-500 font-bold text-sm">Lab Pilot</span>
               </div> 
               { /* <div>
                 <p className="text-gray-800 text-xs font-semibold">Azizul Haque Diagnostic Center and Hospital</p>
