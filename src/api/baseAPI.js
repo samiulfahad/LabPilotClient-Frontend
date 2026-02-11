acc8975d5c851449ed88b7ac80356aee8086ad00
samiulfahad <samiul_fahad@yahoo.com>
2026-02-11 19:19:54 +0600

    CRUD Referrer Design Update

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -1,12 +1,12 @@
 // src/services/api.js
-import axios from 'axios';
+import axios from "axios";
 
-const cloud = "https://labpilot-adminbackend.onrender.com/api/v1"
-const local = 'http://localhost:5000/api/v1'
+const cloud = "https://labpilotclient-backend.onrender.com/api/v1";
+const local = "http://localhost:5000/api/v1";
 
 const api = axios.create({
-  baseURL: local,
-   timeout: 10000,
+  baseURL: cloud,
+  timeout: 10000,
 });
 
 export default api;
